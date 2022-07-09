abf0242716c1d4f32a40610e62aab1e9c6a4bdb0
Shirin Hajahmadi <62059613+shirinhaj@users.noreply.github.com>
2022-07-09 23:58:46 +0200

    Next

---
+++ Mugler/Assets/ResearchCenter/Materials/Walls/TriggerDoorController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoorController : MonoBehaviour
+{
+    [SerializeField] private Animator mydoor = null;
+    [SerializeField] private bool openTrigger = false;
+    [SerializeField] private bool closeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("HitHappened");
+
+        //if(other.CompareTag("Player"))
+        {
+            if(openTrigger)
+            {
+                mydoor.Play("OpenDoor", 0, 0.0f);
+            }
+            else if(closeTrigger)
+            {
+                mydoor.Play("CloseDoor", 0, 0.0f);
+            }
+        }
+    }
+    public void looked()
+    {
+        mydoor.Play("OpenDoor", 0, 0.0f);
+    }    
+}
