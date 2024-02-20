76a7c77b59609d40647ade7501755576af2ff3fa
Shirin Hajahmadi <62059613+shirinhaj@users.noreply.github.com>
2024-02-20 15:00:11 +0100

    Adding the close buttons

---
+++ Mugler/Assets/Scripts/MakeActive.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeActive : MonoBehaviour
+{
+    public GameObject PanelGameObj;
+
+    // Start is called before the first frame update
+
+    public void Open()
+    {
+        PanelGameObj.SetActive(true);
+    }
+
+    public void Close()
+    {
+        PanelGameObj.SetActive(false);
+    }
+
+}
